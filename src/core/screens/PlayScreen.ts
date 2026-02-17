fb81514caec0dc69ee05cbe7dbe04f0c50a2f4a1
Abraham <eshkim1@gmail.com>
2026-02-17 20:34:04 +0100

    Fix PlayScreen

--- src/core/screens/PlayScreen.ts
+++ src/core/screens/PlayScreen.ts
@@ -12,17 +12,24 @@ class PlayScreen implements GameScreen
   }
 
   draw(): void {
-    background(0);
+    // background
+    background(25, 35, 60);
 
+    // demo text
     fill(255);
     textAlign(CENTER, CENTER);
-    textSize(40);
+    textSize(48);
     text("PLAYING", width / 2, height / 2);
+
+    textSize(18);
+    text("Press ESC to pause", width / 2, height / 2 + 60);
   }
+
   keyPressed(code: number): void {
     //press ESC to go back to start menu
     if (code === ESCAPE) {
-      this.game.changeScreen(new StartScreen(this.game));
+      // this.game.changeScreen(new StartScreen(this.game));
+      this.game.changeScreen(new PauseScreen(this.game));
     }
   }
 }
