f825fe2ceda89b1e5531f2d9edc42c066795ae6f
Abraham <eshkim1@gmail.com>
2026-02-23 23:50:29 +0100

    update facing in movement

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -1,107 +1,115 @@
 /// <reference path="entity.ts" />
 
 class Player extends entity {
-    private onGround: boolean = false;
-    private onPlatform: boolean = false;
-    private isFalling: boolean = false;
+  private onGround: boolean = false;
+  private onPlatform: boolean = false;
+  private isFalling: boolean = false;
+  public facing: number = 1; // 1 = right, -1 = left
 
-    constructor(p: p5.Vector, v: p5.Vector, s: p5.Vector, h: number) {
-        super(p, v, s, h, true);
+  constructor(p: p5.Vector, v: p5.Vector, s: p5.Vector, h: number) {
+    super(p, v, s, h, true);
+  }
+
+  public onCollision(other: entity): void {
+    if (other instanceof Platform) {
+      this.handlePlatformLanding(other);
     }
+  }
+  private handlePlatformLanding(other: entity) {
+    if (this.isFalling) return;
 
-    public onCollision(other: entity): void {
-        if (other instanceof Platform) {
-            this.handlePlatformLanding(other);
-        }
+    const platformTop = other.getPosition().y;
+    const isAbovePlatform =
+      this.position.y + this.size.y - this.velocity.y <= platformTop;
+
+    const freeFall = this.velocity.y > 0;
+
+    if (freeFall && isAbovePlatform) {
+      this.position.y = platformTop - this.size.y;
+      this.velocity.y = 0;
+
+      this.onGround = true;
+      this.onPlatform = true;
+      this.isFalling = false;
     }
-    private handlePlatformLanding(other: entity) {
-        if (this.isFalling) return;
+  }
 
-        const platformTop = other.getPosition().y;
-        const isAbovePlatform = this.position.y + this.size.y - this.velocity.y <= platformTop;
+  private takedamage(n: number): void {}
 
-        const freeFall = this.velocity.y > 0;
+  public update(gravity: number, worldWidth: number) {
+    this.move();
+    super.update(gravity, worldWidth);
+    this.updatePosition(worldWidth);
+  }
 
-        if (freeFall && isAbovePlatform) {
-            this.position.y = platformTop - this.size.y;
-            this.velocity.y = 0;
+  private updatePosition(worldWidth: number) {
+    // player is on the ground
+    this.checkIfPlayerIsOnGround();
 
-            this.onGround = true;
-            this.onPlatform = true;
-            this.isFalling = false;
-        }
+    if (this.position.x >= worldWidth - this.size.x) {
+      this.velocity.x = 0;
+      this.position.x = worldWidth - this.size.x;
+      this.onGround = true;
+      this.isFalling = false;
     }
-
-    private takedamage(n: number): void { }
-
-    public update(gravity: number, worldWidth: number) {
-        this.move();
-        super.update(gravity, worldWidth)
-        this.updatePosition(worldWidth);
+    if (this.position.x < 0) {
+      this.velocity.x = 0;
+      this.position.x = 0;
+      this.onGround = true;
+      this.isFalling = false;
     }
-
-    private updatePosition(worldWidth: number) {
-
-        // player is on the ground 
-        this.checkIfPlayerIsOnGround();
-
-        if (this.position.x >= worldWidth - this.size.x) {
-            this.velocity.x = 0;
-            this.position.x = worldWidth - this.size.x;
-            this.onGround = true;
-            this.isFalling = false;
-        }
-        if (this.position.x < 0) {
-            this.velocity.x = 0;
-            this.position.x = 0;
-            this.onGround = true;
-            this.isFalling = false;
-        }
+  }
+  private checkIfPlayerIsOnGround() {
+    if (this.position.y > height - this.size.y) {
+      this.velocity.y = 0;
+      this.position.y = height - this.size.y;
+      this.onGround = true;
+      this.onPlatform = false;
+      this.isFalling = false;
     }
-    private checkIfPlayerIsOnGround() {
-        if (this.position.y > height - this.size.y) {
-            this.velocity.y = 0;
-            this.position.y = height - this.size.y;
-            this.onGround = true;
-            this.onPlatform = false;
-            this.isFalling = false;
-        }
-    }
-    private move() {
-        this.velocity.x = 0;
+  }
+  private move() {
+    this.velocity.x = 0;
 
-        if (keyIsDown(65)) { // a
-            this.velocity.x = -5;
-        }
-        if (keyIsDown(68)) { // d
-            this.velocity.x = 5;
-        }
-        if (keyIsDown(83) && this.onPlatform) { // s
-            this.velocity.y = 0.8;
-            this.onGround = false;
-            this.onPlatform = false;
-            this.isFalling = true;
-        }
-        if (keyIsDown(32)) { // space
-            this.jump();
-        }
-        if (keyIsDown(75)) { // k
-            this.entityDamage(3.33);
-        }
+    if (keyIsDown(65)) {
+      // a
+      this.velocity.x = -5;
+      this.facing = -1;
     }
-    private jump() {
-        if (this.onGround) {
-            this.velocity.y = -30;
-            this.onGround = false;
-            this.onPlatform = false;
-        }
+    if (keyIsDown(68)) {
+      // d
+      this.velocity.x = 5;
+      this.facing = 1;
     }
+    if (keyIsDown(83) && this.onPlatform) {
+      // s
+      this.velocity.y = 0.8;
+      this.onGround = false;
+      this.onPlatform = false;
+      this.isFalling = true;
+    }
+    if (keyIsDown(32)) {
+      // space
+      this.jump();
+    }
+    if (keyIsDown(75)) {
+      // k
+      this.entityDamage(3.33);
+    }
+  }
+  private jump() {
+    if (this.onGround) {
+      this.velocity.y = -30;
+      this.onGround = false;
+      this.onPlatform = false;
+    }
+  }
 
-    // public overlaps(other: Entity) {
-    //     if (other instanceof Platform) {
-    //         // Speciell lösning
-    //     } else {
-    //         super.overlaps(other);
-    //     }
-    // }
+  // public overlaps(other: Entity) {
+  //     if (other instanceof Platform) {
+  //         // Speciell lösning
+  //     } else {
+  //         super.overlaps(other);
+  //     }
+  // }
 }
