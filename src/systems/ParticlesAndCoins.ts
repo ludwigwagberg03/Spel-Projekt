37d649d7529c7b411929059bbd2e8385326dc151
Abraham <eshkim1@gmail.com>
2026-02-25 18:17:12 +0100

    add explosion particle movement, gravity and fade animation

--- src/systems/ParticlesAndCoins.ts
+++ src/systems/ParticlesAndCoins.ts
@@ -1,5 +1,5 @@
 // ================================
-// Particles + Coins 
+// Particles + Coins
 // ================================
 
 class ExplosionParticle {
@@ -15,4 +15,39 @@ class ExplosionParticle {
     this.size = random(3, 8);
   }
 
+  update(): void {
+    this.life -= deltaTime;
+
+    // little gravity
+    this.vel.y += 0.12;
+
+    // move
+    this.pos.add(this.vel);
+
+    // slow down
+    this.vel.mult(0.92);
+  }
+  draw(): void {
+    if (this.life <= 0) return;
+
+    push();
+    noStroke();
+
+    const a = map(this.life, 0, 350, 0, 200);
+    fill(255, 170, 60, a);
+
+    ellipse(this.pos.x, this.pos.y, this.size);
+    pop();
+  }
+
+  get alive(): boolean {
+    return this.life > 0;
+  }
+
+
+
+
+
+
+  
 }
